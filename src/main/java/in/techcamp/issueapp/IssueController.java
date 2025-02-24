fbedeab2f1638437e439e0182add08267b0c5f38
9kuwa0 <techkuwa@gmail.com>
2025-02-24 22:30:47 +0900

    記入忘れ入力

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -2,6 +2,7 @@ package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
